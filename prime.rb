32f0b425a5d896f2a6eb1ea6826fc93cc4406f1a
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-06 19:21:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  start = 2 
+  if num > 1 
+    range = (start...num-1).to_a 
+    range.none? do |test_num|
+      num % test_num === 0 
+    end
+  else 
+    false 
+  end
+end
+  
